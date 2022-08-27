a1b61f759204a8ad96612340d2f7535a51a4178e
Eric Wan <ericwan808@gmail.com>
2022-08-27 12:54:14 -0500

    port env var bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const fs = require('fs');
 const cors = require("cors");
 
@@ -12,7 +12,7 @@ app.use(cors());
 if(true){
     app.use(express.static(path.join(__dirname, '/client/build')));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname,'client','build','index.html'));
     })
 }
