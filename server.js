535cf1f59a41aea0265609b9c993c2d875b86a04
Eric Wan <ericwan808@gmail.com>
2022-08-28 18:04:52 -0500

    e

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-if(false){
+if(true){
     app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('/', (req, res) => {
